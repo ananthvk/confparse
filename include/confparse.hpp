082bae59100b55c0a22605030a93c119aba21218
ananthvk <mailstoanantha@gmail.com>
2024-12-07 02:20:38 +0530

    add config object

--- include/confparse.hpp
+++ include/confparse.hpp
@@ -1,8 +1,87 @@
 #ifndef A_CONFPARSE_H
 #define A_CONFPARSE_H
+#include <fstream>
+#include <map>
+#include <sstream>
+#include <variant>
 
 namespace confparse
 {
-inline int add(int a, int b) { return a + b; }
+
+/**
+ * @brief This class represents a value in the configuration, it can either be an integer, double or
+ * a string
+ */
+class ValueType
+{
+    std::variant<std::monostate, long long int, long double, std::string> val;
+    bool is_empty_;
+
+  public:
+    ValueType() : is_empty_(true) {}
+
+    template <typename T> ValueType(const T &value) : val{value}, is_empty_{false} {}
+
+    bool is_empty() const { return is_empty_; }
+
+    std::string as_string() const { return std::get<std::string>(val); }
+
+    long long int as_int() const { return std::get<long long int>(val); }
+
+    long double as_real() const { return std::get<long double>(val); }
+};
+
+class Config
+{
+  public:
+    const ValueType &get(const std::string &key) const
+    {
+        auto iter = cfg_map.find(key);
+        if (iter == cfg_map.end())
+        {
+            return empty_value;
+        }
+        return iter->second;
+    }
+
+    template <typename T> void set(const std::string &key, const T &value)
+    {
+        cfg_map[key] = ValueType{value};
+    }
+
+    size_t size() const { return cfg_map.size(); }
+
+    ValueType &operator[](const std::string &key) { return cfg_map[key]; }
+
+    void erase(const std::string &key)
+    {
+        auto iter = cfg_map.find(key);
+        if (iter != cfg_map.end())
+        {
+            cfg_map.erase(iter);
+        }
+    }
+
+  private:
+    ValueType empty_value;
+    std::map<std::string, ValueType> cfg_map;
+};
+
+/*
+inline Config from_stream(std::istream &is) {}
+
+inline Config from_str(const std::string &s)
+{
+    std::istringstream iss(s);
+    return from_stream(iss);
+}
+
+inline Config from_file(const std::string &filepath)
+{
+    std::ifstream ifs(filepath);
+    return from_stream(ifs);
+}
+*/
+
 } // namespace confparse
 #endif // A_CONFPARSE_H
--- tests/confparse_test.cpp
+++ tests/confparse_test.cpp
@@ -1,25 +1,47 @@
 #include "confparse.hpp"
 #include <gtest/gtest.h>
 
-TEST(TestConparse, Addition)
+TEST(TestConfig, EmptyConfig)
 {
-    auto result = confparse::add(3, 5);
-    ASSERT_EQ(result, 8);
-    result = confparse::add(4, 5);
-    ASSERT_EQ(result, 9);
-    result = confparse::add(5, 5);
-    ASSERT_EQ(result, 10);
+    confparse::Config cfg;
+    ASSERT_EQ(cfg.size(), 0);
 }
 
-TEST(TestConfparse, Addition2)
+TEST(TestConfig, GetAndSet)
 {
-    auto result = confparse::add(3, 5);
-    ASSERT_EQ(result, 8);
-    result = confparse::add(4, 5);
-    ASSERT_EQ(result, 9);
-    result = confparse::add(5, 5);
-    ASSERT_EQ(result, 10);
+    confparse::Config cfg;
+    ASSERT_EQ(cfg.get("non existent key").is_empty(), true);
+    ASSERT_EQ(cfg.get("").is_empty(), true);
+    ASSERT_EQ(cfg.get("Some other key").is_empty(), true);
+    
+    cfg.set("KEY1", "This is the value for this key");
+    cfg.set("KEY2", "");
+    cfg.set("KEY3", 123456789);
+    cfg.set("KEY4", 3.1415);
+
+    ASSERT_EQ(cfg.get("KEY3").as_int(), 123456789);
+    ASSERT_EQ(cfg.get("KEY1").as_string(), "This is the value for this key");
+    ASSERT_EQ(cfg.get("KEY2").as_string(), "");
+    ASSERT_NEAR(cfg.get("KEY4").as_real(), 3.1415, 0.001);
+    
+    ASSERT_EQ(cfg.size(), 4);
+    
+    cfg.set("KEY4", "this is a string");
+    ASSERT_EQ(cfg.get("KEY4").as_string(), "this is a string");
+    ASSERT_EQ(cfg.get("KEY4").is_empty(), false);
+    
+    cfg.erase("KEY4");
+    ASSERT_EQ(cfg.get("KEY4").is_empty(), true);
+    
+    cfg["floatkey"] = 3.89;
+    cfg["numkey"] = 3;
+    cfg["strkey"] = "This is string!!";
+
+    ASSERT_NEAR(cfg.get("floatkey").as_real(), 3.89, 0.01);
+    ASSERT_EQ(cfg.get("numkey").as_int(), 3);
+    ASSERT_EQ(cfg.get("strkey").as_string(), "This is string!!");
 }
+
 
 int main(int argc, char *argv[])
 {
